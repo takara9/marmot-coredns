9fffc938adb2335ae30c0a0e6b9dd25957e1935f
Maho Takara <takara9@gmail.com>
2021-02-26 23:56:58 +0900

    update

--- playbook/ca/templates/cp_ca_htdocs.sh
+++ playbook/ca/templates/cp_ca_htdocs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 . environment
 cp  "$CA_HOME/ca.crt" "$CRT_HOME/ca.pem"
+chmod a+r "$CRT_HOME/ca.pem"
 openssl x509 -text -fingerprint -noout -in "$CRT_HOME/ca.pem" >  "${WORK_HOME}/ca_pem.txt"
+
