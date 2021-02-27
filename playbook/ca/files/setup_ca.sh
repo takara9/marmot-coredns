6493b515f4a486688fd6d606d382987daa1b2391
Maho Takara <takara9@gmail.com>
2021-02-28 08:41:05 +0900

    update

--- playbook/ca/files/setup_ca.sh
+++ playbook/ca/files/setup_ca.sh
@@ -11,7 +11,7 @@
 # L   市区町村名     例 Nihonbash,Hakozaki-cho,Chuo-ku
 # ST  都道府県名     例 Tokyo
 # C   国             例 JP
-CA_SUBJ="/C=JP/L=Nihombash,ST=Tokyo/OU=CA/O=Home Labo/CN=ca.labo.local"
+CA_SUBJ="/C=JP/ST=Tokyo/OU=Certification Authority Services/O=Home Labo/CN=Home Labo Environment"
 
 #
 # 認証局の開設時のみ必要
@@ -23,7 +23,7 @@ if [ -d $CA_HOME ]; then
 else
     mkdir -p $CA_HOME
     openssl genrsa -out "$CA_HOME/ca.key" 4096
-    openssl req -x509 -new -nodes -sha512 -days 3650 \
+    openssl req -x509 -new -nodes -sha512 -days 4000 \
         -subj "$CA_SUBJ" \
         -key "$CA_HOME/ca.key" \
         -out "$CA_HOME/ca.crt"
