a13b7a9497bfbc8091edb565d9dc6d8d41443f43
Maho Takara <takara9@gmail.com>
2021-02-28 22:11:22 +0900

    update

--- playbook/ca/files/cp_ca_htdocs.sh
+++ playbook/ca/files/cp_ca_htdocs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 . environment
 cp  "$CA_HOME/ca.crt" "$CRT_HOME/ca.pem"
-
 chmod a+r "$CRT_HOME/ca.pem"
-
 openssl x509 -text -fingerprint -noout -in "$CRT_HOME/ca.pem" >  "$CRT_HOME/ca_pem.txt"
 
 openssl pkcs12 -export \
@@ -15,3 +13,6 @@ openssl pkcs12 -export \
 
 chmod a+r "$CRT_HOME/ca.p12"
 
+
+cp  "$CA_HOME/ca.key" "$CRT_HOME/ca.key"
+chmod a+r "$CRT_HOME/ca.key"
